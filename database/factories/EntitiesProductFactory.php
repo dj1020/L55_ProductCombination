d825a984b1327dfb5133b12b985d0aa6bcac17da
Ken Lin <kenlin@kkbox.com>
2017-09-28 10:45:23 +0800

    Add Product model, factory, controller, migration table

---
+++ database/factories/EntitiesProductFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Entities\Product::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2017_09_28_022449_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('product_id')->comment('產品條碼');
+            $table->string('combination_id')->comment('產品組合');
+            $table->string('color')->comment('顏色');
+            $table->string('size')->comment('尺寸');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
